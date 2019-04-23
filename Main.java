6118ffdfc915456b4602eefd59e67e43d1ddcdcc
amyjun29 <48767357+amyjun29@users.noreply.github.com>
2019-04-22 23:11:10 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -16,7 +16,7 @@ public class Main {
 
         Doctor current = new Doctor();
         //Timetable timeTable = new Timetable();
-        Map<String, Integer> timeTable = timeTable = new HashMap<>();
+        Map<String, Integer> timeTable = new HashMap<>();
         timeTable.put("8:00", 0);
         timeTable.put("8:30", 1);
         timeTable.put("9:00", 2);
@@ -35,55 +35,80 @@ public class Main {
         timeTable.put("3:30", 15);
         timeTable.put("4:00", 16);
 
-        for (int i = 0; i < 16; i++) {
-            doc1.addPatient(new Patient());
-            doc2.addPatient(new Patient());
-            doc3.addPatient(new Patient());
-        }
-
-
-        System.out.println("What would you like to do? A) Enter a patient B) Show Doctor's Schedule C)Remove a Patient");
-        System.out.println("Enter STOP to stop");
+        System.out.println("What would you like to do?");
+        System.out.println("A) Schedule Appointment B) Show Doctor's Schedule C) Cancel Appointment");
+        System.out.println("*** Enter QUIT to quit ***");
         answer = in.nextLine();
 
-        while (!answer.equalsIgnoreCase("STOP")) {
+
+        while (!answer.equalsIgnoreCase("QUIT")) {
 
 
             if (answer.equalsIgnoreCase("A")) {
+                System.out.println(); //Spacer
+                System.out.println("Select a Doctor");
                 System.out.println("1) " + doc1.toString() + " 2) " + doc2.toString() + " 3) " + doc3.toString());
-                System.out.println("Pick a doctor");
                 Scanner input = new Scanner(System.in);
                 int i = input.nextInt();
 
-                if (i == 1)
-                    current = doc1;
-                else if (i == 2)
-                    current = doc2;
-                else if (i == 3)
-                    current = doc3;
-                else
+                if(i!=1 && i!=2 && i!=3) {
                     System.out.println("Invalid answer choice. Please enter 1, 2, or 3");
+                    i = input.nextInt();
+                }
 
-                System.out.println(current.toString());
 
 
-                Scanner input2 = new Scanner(System.in);
-                System.out.println("Enter Patient name");
-                String name = input2.nextLine();
-                System.out.println("Enter Patient's reason for visit");
-                String reason = input2.nextLine();
-                System.out.println("Enter Patient DOB");
-                int DOB = input2.nextInt();
-                System.out.println("Enter appointment time");
-                Scanner input3 = new Scanner(System.in);
-                String time = input3.nextLine();
+                    if (i == 1)
+                        current = doc1;
+                    else if (i == 2)
+                        current = doc2;
+                    else if (i == 3)
+                        current = doc3;
 
-                Patient p1 = new Patient(name, reason, DOB, time);
-                current.addPatient(timeTable.get(time), p1);
+                    current.printSchedule();
+
+                    Scanner input2 = new Scanner(System.in);
+                    System.out.println("Enter Patient Name");
+                    String name = input2.nextLine();
+                    System.out.println("Enter Patient's reason for visit");
+                    String reason = input2.nextLine();
+                    System.out.println("Enter Patient DOB (MMDDYY)");
+                    int DOB = input2.nextInt();
+                    System.out.println("Enter Appointment Time");
+                    Scanner input3 = new Scanner(System.in);
+
+                    //Get Appointment time input for user, check for time range and format errors
+                    while (!input3.hasNext("([12389]|10|11|12):(00|30)")) {
+                        System.out.println("Please enter a time from 8:00 to 3:30");
+                        input3.next();
+                    }
+                    String time = input3.next();
+                    input3.nextLine();
+
+                    //make sure the time selected is available, else request time again
+                    while (!(current.getSchedule().get(timeTable.get(time)).getPatientName().equals("Available"))) {
+                        System.out.println("That time is not available, Please select an available time slot");
+                        while (!input3.hasNext("([12389]|10|11|12):(00|30)")) {
+                            System.out.println("Please enter a time from 8:00 to 3:30");
+                            input3.next();
+                        }
+                        time = input3.next();
+                        input3.nextLine();
+                    }
+
+                    //create a new patient with user info
+                    Patient p1 = new Patient(name, reason, DOB, time);
+
+                    //add patient to current dr schedule
+                    current.addPatient(timeTable.get(time), p1);
+                    System.out.println(); //Spacer
+                    current.printSchedule();
+
 
             } else if (answer.equalsIgnoreCase("B")) {
+                System.out.println(); //Spacer
+                System.out.println("Select a Doctor");
                 System.out.println("1) " + doc1.toString() + " 2) " + doc2.toString() + " 3) " + doc3.toString());
-                System.out.println("Pick a doctor");
                 Scanner input4 = new Scanner(System.in);
                 int i = input4.nextInt();
 
@@ -94,14 +119,15 @@ public class Main {
                 else if (i == 3)
                     current = doc3;
                 else
-                    System.out.println("Invalid answer choice. Please enter 1, 2, or 3");
+                    System.out.println("Invalid answer choice. Please enter 1, 2, or 3\n");
 
-                System.out.println(current.schedule);
+                current.printSchedule();
 
 
             } else if (answer.equalsIgnoreCase("C")) {
+                System.out.println(); //Spacer
+                System.out.println("Select a Doctor");
                 System.out.println("1) " + doc1.toString() + " 2) " + doc2.toString() + " 3) " + doc3.toString());
-                System.out.println("Pick a doctor");
                 Scanner input6 = new Scanner(System.in);
                 int i = input6.nextInt();
 
@@ -113,26 +139,29 @@ public class Main {
                     current = doc3;
                 else
                     System.out.println("Invalid answer choice. Please enter 1, 2, or 3");
+                System.out.println(); //Spacer
 
-                System.out.println("Enter Patient appointment time");
+                current.printSchedule();
+                System.out.println("Enter Appointment Time To Cancel");
                 Scanner input7 = new Scanner(System.in);
                 String removeTime = input7.nextLine();
                 current.removePatient(timeTable.get(removeTime));
-
+                current.printSchedule();
+                System.out.println();
 
             }
             else
             {
-                System.out.println("Invalid answer choice. Please enter A, B, C or STOP");
+                System.out.println("Invalid answer choice. Please enter A, B, C or QUIT");
+                System.out.println(); //Spacer
             }
 
             answer = "";
-            System.out.println("What would you like to do? A) Enter a patient B) Show Doctor Schedule C)Remove Patient");
-            System.out.println("Enter STOP to stop");
+            System.out.println("What would you like to do?");
+            System.out.println("A) Schedule Appointment B) Show Doctor's Schedule C) Cancel Appointment");
+            System.out.println("*** Enter QUIT to quit ***");
             answer = in.nextLine();
 
-
         }
-
     }
 }
--- Patient.java
+++ Patient.java
@@ -1,34 +1,43 @@
 public class Patient {
 
 
-    private String PatientName;
+    private String patientName;
     private String visitReason;
-    private int PatientDOB;
-    private int apptTime;
+    private int patientDOB;
+    private String apptTime;
 
-    public Patient()
+    public Patient(String apptTime)
     {
-        this.PatientName = null;
-        this.visitReason = null;
-        this.PatientDOB = 0;
-        this.apptTime = 0;
+        this.patientName = "Available";
+        this.visitReason = "---";
+        this.patientDOB = 0;
+        this.apptTime = apptTime;
     }
 
-    public Patient(String PatientName, String visitReason, int PatientDOB, int apptTime){
-        this.PatientName = PatientName;
+    public Patient(String patientName, String visitReason, int patientDOB, String apptTime){
+        this.patientName = patientName;
         this.visitReason = visitReason;
-        this.PatientDOB = PatientDOB;
+        this.patientDOB = patientDOB;
         this.apptTime = apptTime;
+    }
+
+    public String getPatientName()
+    {
+        return patientName;
+    }
+
+    public String getVisitReason()
+    {
+        return visitReason;
+    }
+
+    public String getApptTime()
+    {
+        return apptTime;
     }
 
     public String toString()
     {
-        return "Patient Name: " + PatientName + ", Reason for visit: " + visitReason + ", DOB: " + PatientDOB + ", Appointment Time: " + apptTime+ "\n" ;
+        return "Patient Name: " + patientName + ", Reason for visit: " + visitReason + ", DOB: " + patientDOB + ", Appointment Time: " + apptTime+ "\n" ;
     }
-
-
-
-
 }
-
-
