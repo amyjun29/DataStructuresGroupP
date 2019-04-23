5ad7a484766bd850e9181882b01a67fe2a281319
amyjun29 <48767357+amyjun29@users.noreply.github.com>
2019-04-22 23:18:49 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -10,12 +10,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //create three doctors
         Doctor doc1 = new Doctor("Dr.Brown", 2305);
         Doctor doc2 = new Doctor("Dr.Gabriel", 1005);
         Doctor doc3 = new Doctor("Dr.Cho", 1010);
 
         Doctor current = new Doctor();
-        //Timetable timeTable = new Timetable();
+
+        //create time table for doctor's schedule
+
         Map<String, Integer> timeTable = new HashMap<>();
         timeTable.put("8:00", 0);
         timeTable.put("8:30", 1);
@@ -35,22 +38,28 @@ public class Main {
         timeTable.put("3:30", 15);
         timeTable.put("4:00", 16);
 
+
+        //prompt user and get user input
         System.out.println("What would you like to do?");
         System.out.println("A) Schedule Appointment B) Show Doctor's Schedule C) Cancel Appointment");
         System.out.println("*** Enter QUIT to quit ***");
         answer = in.nextLine();
 
 
+        //loop while the user input isn't quit
         while (!answer.equalsIgnoreCase("QUIT")) {
 
 
             if (answer.equalsIgnoreCase("A")) {
-                System.out.println(); //Spacer
+                System.out.println();
+
+                //doctor options
                 System.out.println("Select a Doctor");
                 System.out.println("1) " + doc1.toString() + " 2) " + doc2.toString() + " 3) " + doc3.toString());
                 Scanner input = new Scanner(System.in);
                 int i = input.nextInt();
 
+                //If user inputs invalid answer, prompt user again
                 if(i!=1 && i!=2 && i!=3) {
                     System.out.println("Invalid answer choice. Please enter 1, 2, or 3");
                     i = input.nextInt();
@@ -58,6 +67,7 @@ public class Main {
 
 
 
+                //select doctor based on the user input
                     if (i == 1)
                         current = doc1;
                     else if (i == 2)
@@ -67,6 +77,8 @@ public class Main {
 
                     current.printSchedule();
 
+
+                    //get user input for patient information
                     Scanner input2 = new Scanner(System.in);
                     System.out.println("Enter Patient Name");
                     String name = input2.nextLine();
@@ -96,7 +108,7 @@ public class Main {
                         input3.nextLine();
                     }
 
-                    //create a new patient with user info
+                    //create a new patient with user input
                     Patient p1 = new Patient(name, reason, DOB, time);
 
                     //add patient to current dr schedule
@@ -106,56 +118,77 @@ public class Main {
 
 
             } else if (answer.equalsIgnoreCase("B")) {
-                System.out.println(); //Spacer
+                System.out.println();
+
+                //doctor options
                 System.out.println("Select a Doctor");
                 System.out.println("1) " + doc1.toString() + " 2) " + doc2.toString() + " 3) " + doc3.toString());
                 Scanner input4 = new Scanner(System.in);
                 int i = input4.nextInt();
 
+                //If user inputs invalid answer, prompt user again
+                if(i!=1 && i!=2 && i!=3) {
+                    System.out.println("Invalid answer choice. Please enter 1, 2, or 3");
+                    i = input4.nextInt();
+                }
+
+                //select doctor based on the user input
                 if (i == 1)
                     current = doc1;
                 else if (i == 2)
                     current = doc2;
                 else if (i == 3)
                     current = doc3;
-                else
-                    System.out.println("Invalid answer choice. Please enter 1, 2, or 3\n");
 
+
+                //show selected doctor's schedule
                 current.printSchedule();
 
 
             } else if (answer.equalsIgnoreCase("C")) {
-                System.out.println(); //Spacer
+                System.out.println();
                 System.out.println("Select a Doctor");
                 System.out.println("1) " + doc1.toString() + " 2) " + doc2.toString() + " 3) " + doc3.toString());
                 Scanner input6 = new Scanner(System.in);
                 int i = input6.nextInt();
 
+                //If user inputs invalid answer, prompt user again
+                if(i!=1 && i!=2 && i!=3) {
+                    System.out.println("Invalid answer choice. Please enter 1, 2, or 3");
+                    i = input6.nextInt();
+                }
+
                 if (i == 1)
                     current = doc1;
                 else if (i == 2)
                     current = doc2;
                 else if (i == 3)
                     current = doc3;
-                else
-                    System.out.println("Invalid answer choice. Please enter 1, 2, or 3");
-                System.out.println(); //Spacer
+
+                System.out.println();
 
                 current.printSchedule();
+
+                //get user input for time selection
                 System.out.println("Enter Appointment Time To Cancel");
                 Scanner input7 = new Scanner(System.in);
                 String removeTime = input7.nextLine();
+
+                //remove patient
                 current.removePatient(timeTable.get(removeTime));
                 current.printSchedule();
                 System.out.println();
 
             }
+
+            //if the user input isn't A,B, or C prompt user again
             else
             {
                 System.out.println("Invalid answer choice. Please enter A, B, C or QUIT");
                 System.out.println(); //Spacer
             }
 
+            //prompting user for input to either start or stop the loop
             answer = "";
             System.out.println("What would you like to do?");
             System.out.println("A) Schedule Appointment B) Show Doctor's Schedule C) Cancel Appointment");
